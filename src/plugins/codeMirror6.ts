bf370cb51c6905081fec5b61e653dc4f0c3f1500
Nelson Job <njobnz@proton.me>
2024-12-09 00:44:22 +1300

    feat: support autocompleting token renderer tags from dropdown list
    the autocomplete dropdown will continue to function when a renderer
    tag is entered as the first character after the token tag
    
    the closing renderer tag is included in the autocomplete string when
    an item is selected from the list

--- src/plugins/codeMirror6.ts
+++ src/plugins/codeMirror6.ts
@@ -12,20 +12,24 @@ export default async (CodeMirror: any, _context: ContentScriptContext) => {
     require('@codemirror/autocomplete') as typeof CodeMirrorAutocompleteType;
 
   const completeToken = async (context: CompletionContext): Promise<CompletionResult> => {
+    const brackets = { '[': ']', '{': '}', '(': ')' };
     const { prefix, suffix, idOnly, autocomplete } = settings();
+
     if (!autocomplete) return null;
 
-    // prettier-ignore
-    const wrap = i => i.split('').map(c => `[${escape(c)}]`).join('');
-    const pattern = new RegExp(`${wrap(prefix)}[[|{|(]?[^\n]*`);
+    const pattern = new RegExp(`${escape(prefix)}[^]*`);
     const match = context.matchBefore(pattern);
 
     if (!match || (match.from === match.to && !context.explicit)) return null;
 
+    const first = match.text[prefix.length];
+    const opening = /^[{[(]/.test(first) ? first : '';
+    const closing = brackets[opening] ?? '';
+
     const tokens = await _context.postMessage({
       command: getFilteredTokensCmd,
       query: {
-        prefix: match.text.substring(prefix.length),
+        prefix: match.text.substring(prefix.length + (opening ? 1 : 0)),
       },
     });
 
@@ -33,10 +37,9 @@ export default async (CodeMirror: any, _context: ContentScriptContext) => {
 
     const createApplyCompletionFn = (noteTitle: string, noteId: string) => {
       return (view: EditorView, _completion: Completion, from: number, to: number) => {
-        // TODO: Replace idOnly here with user choice of note id or title from autocomplete dropdown
-        // TODO: Let the user select the renderer before inserting the token
+        // TODO: Add custom keybind to insert note id from autocomplete dropdown
         const tokenName = idOnly ? noteId : noteTitle;
-        const tokenText = `${prefix}${tokenName}${suffix}`;
+        const tokenText = `${prefix}${opening}${tokenName}${closing}${suffix}`;
         view.dispatch(insertCompletionText(view.state, tokenText, from, to));
       };
     };
