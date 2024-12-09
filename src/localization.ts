d8bbcc1ae3d950b0f7f1fab0e6c2a23dc25e4fe3
Nelson Job <njobnz@proton.me>
2024-12-10 01:12:17 +1300

    docs: minor settings description updates

--- src/localization.ts
+++ src/localization.ts
@@ -27,19 +27,19 @@ const defaultStrings: AppLocalization = {
     'Display a dropdown of token suggestions in the editor when the opening tag is entered.',
   setting__idOnly: 'Note IDs only',
   setting__idOnly__description: 'Allow only note IDs to be used as tokens.',
-  setting__tag: 'Tag Filter',
+  setting__tag: 'Tag filter',
   setting__tag__description:
-    'Filter tokenizable note titles to the specified tag name. Notes without this tag can still be reference using their note ID.',
+    'Filter tokens to the specified tag name. Notes without this tag can still be reference using their note ID.',
   setting__prefix: 'Token prefix',
   setting__prefix__description: 'Opening tag for tokens (default: %%).',
   setting__suffix: 'Token suffix',
   setting__suffix__description: 'Closing tag for tokens (default: %%).',
   setting__blockFence: 'Always replace in code blocks',
   setting__blockFence__description:
-    'Automatically replace tokens in block code fences (```). Write `tokenized` in the block header to replace tokens in that block when disabled.',
+    'Automatically replace tokens in fenced code blocks (```). Write `tokenized` in the block header to replace tokens in that block when disabled.',
   setting__renderMarkdown: 'Always render markdown (caution)',
   setting__renderMarkdown__description:
-    'Always render markdown found in tokenized notes without using ( and ) tags. Markdown in code blocks is not rendered. This feature is highly experimental and may cause markdown rendering issues.',
+    'Always render markdown found in referenced notes without using ( ) tags. Markdown is not rendered in code blocks. This feature is highly experimental and may cause rendering issues.',
 };
 
 const localizations: Record<string, AppLocalization> = {
